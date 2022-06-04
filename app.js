f5ed3dafc58e9df802cc26f5de879a10129e2ac5
Roger Zeni Antunes <rogerzeniantunes@gmail.com>
2022-06-04 15:28:47 -0300

    desafio quiz

---
+++ app.js
@@ -0,0 +1,40 @@
+const form = document.querySelector('.quiz-form')
+const result = document.querySelector('.popup-wrapper')
+const span = document.querySelector('.text-primary')
+const closePopup = document.querySelector('.popup-close')
+const recomecar = document.querySelector('.recomecar')
+
+
+const respostasCertas = ['B', 'B', 'B', 'B']
+
+form.addEventListener('submit', event => {
+   event.preventDefault()
+
+   let score = 0
+    const userRespostas=[
+        form.inputQuestion1.value,
+        form.inputQuestion2.value,
+        form.inputQuestion3.value,
+        form.inputQuestion4.value,
+    ]
+
+    userRespostas.forEach( (item, index) => {
+        if(item === respostasCertas[index]){
+            score += 25
+        }
+    })
+    result.classList.remove('d-none')
+    span.textContent = `${score}%`
+})
+
+closePopup.addEventListener('click', event => {
+    event.preventDefault()
+    result.classList.add('d-none')
+})
+
+
+recomecar.addEventListener('click', function(){
+   location.reload()
+})
+
+console.log(recomecar)
